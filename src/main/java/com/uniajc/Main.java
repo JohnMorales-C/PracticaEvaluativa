784259ad5536d4a1bbde9b6b23d5c4179defbed8
Gabriel Perez <gperezm@profesores.uniajc.edu.co>
2026-03-27 20:16:13 -0500

    Probando nuestra arquitectura mvc para estudiante

--- src/main/java/com/uniajc/Main.java
+++ src/main/java/com/uniajc/Main.java
@@ -1,7 +1,31 @@
 package com.uniajc;
 
+import com.uniajc.controlador.ControladorEstudiante;
+import com.uniajc.modelo.Estudiante;
+import com.uniajc.vista.VistaEstudiante;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Practica MVC - UNIAJC");
+
+        // System.out.println("Creando el modelo...");
+        Estudiante estudiante = new Estudiante("Juan Perez", 20);
+        
+        // System.out.println("Creando la vista...");
+        VistaEstudiante vista = new VistaEstudiante();
+
+        // System.out.println("Creando el controlador...");
+        ControladorEstudiante controlador = new ControladorEstudiante(estudiante, vista);
+
+        // System.out.println("Actualizando la vista...");
+        controlador.actualizarVista();
+        
+        // System.out.println("Modificando el modelo...");
+        estudiante.setNombre("María López");
+        estudiante.setEdad(22);
+
+        // System.out.println("Actualizando la vista después de modificar el modelo...");
+        controlador.actualizarVista();
+
     }
 }
