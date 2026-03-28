4f5e7f37d1add00dde9f7e5e36cb77d8e2f9c30b
Gabriel Perez <gperezm@profesores.uniajc.edu.co>
2026-03-27 20:33:04 -0500

    Agregamos un nuevo metodo para agregar un estudiante a la lista

--- src/main/java/com/uniajc/controlador/ControladorEstudiante.java
+++ src/main/java/com/uniajc/controlador/ControladorEstudiante.java
@@ -1,16 +1,21 @@
 package com.uniajc.controlador;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.uniajc.modelo.Estudiante;
 import com.uniajc.vista.VistaEstudiante;
 
 public class ControladorEstudiante {
 
     private Estudiante estudiante;
+    private List<Estudiante> estudiantes; // Para manejar múltiples estudiantes
     private VistaEstudiante vista;
 
     public ControladorEstudiante(Estudiante estudiante, VistaEstudiante vista) {
         this.estudiante = estudiante;
         this.vista = vista;
+        this.estudiantes = new ArrayList<Estudiante>(); // Inicializar la lista de estudiantes
     }
 
     public Estudiante getEstudiante() {
@@ -29,8 +34,17 @@ public class ControladorEstudiante {
         this.vista = vista;
     }
 
+    public void agregarEstudiante(Estudiante estudiante) {
+        estudiantes.add(estudiante);
+        System.out.println("Estudiante agregado: " + estudiante.getNombre());
+    }
+
     public void actualizarVista() {
         vista.mostrarDetallesEstudiante(estudiante);
+    }
+
+    public void mostrarTodosLosEstudiantes() {
+        vista.mostrarTodosLosEstudiantes(estudiantes);
     }
 
 }
